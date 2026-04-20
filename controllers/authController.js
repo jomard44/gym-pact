d5f40057e93b8d8b5144c0cdcffbe6dacbab05fb
jomard <jomrdali44@gmail.com>
2026-04-20 14:20:32 -0300

    feat: implement authentcation with password encryption (backend)

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,7 +1,28 @@
+import User from "../models/user.js";
+import bcrypt from "bcrypt";
 
-export const register = async (req,res)=>{
-    res.send("register")
-}
-export const login = async (req,res)=>{
-    res.send("login")
-}
+export const register = async (req, res) => {
+  const { email, password } = req.body;
+
+  const existingUser = await User.findOne({ email });
+  if (existingUser) {
+    return res.status(500).json({ message: "user already exist" });
+  }
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  const user = await User.create({ email, password: hashedPassword });
+  res.status(201).json({ message: "Registered" });
+};
+export const login = async (req, res) => {
+  const { email, password } = req.body;
+  
+  const existingUser = await User.findOne({ email });
+  if (!existingUser) {
+    return res.status(400).json({ message: "wrong email or password" });
+  }
+  const isMatch = await bcrypt.compare(password, existingUser.password);
+  if (!isMatch) {
+    return res.status(400).json({ message: "wrong email or password" });
+  }
+  res.status(200).json({ message: "LoggedIn" });
+};
--- server.js
+++ server.js
@@ -1,11 +1,15 @@
-import express from "express"
-import dotenv from "dotenv"
-dotenv.config()
-import router from "./routes/router.js"
+import express from "express";
+import dotenv from "dotenv";
+dotenv.config();
+import router from "./routes/router.js";
+import mongoose from "mongoose";
 
-const app = express()
-app.use("/api",router)
+const app = express();
+app.use(express.json())
+app.use("/api", router);
 
-app.listen(process.env.PORT,()=>{
-    console.log("server is runing on port",process.env.PORT)
-})
+mongoose.connect(process.env.MONGO).then(
+  app.listen(process.env.PORT, () => {
+    console.log("server is runing on port", process.env.PORT);
+  })
+);
