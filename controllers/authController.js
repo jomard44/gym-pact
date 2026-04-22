a8a58fdc47cfe284956ff57511f966f7aeec2b49
jomard <jomrdali44@gmail.com>
2026-04-22 13:56:04 -0300

    feat: create all crud ops for pacts (backend)

--- controllers/authController.js
+++ controllers/authController.js
@@ -2,27 +2,35 @@ import User from "../models/user.js";
 import bcrypt from "bcrypt";
 
 export const register = async (req, res) => {
-  const { email, password } = req.body;
+  try {
+    const { email, password } = req.body;
 
-  const existingUser = await User.findOne({ email });
-  if (existingUser) {
-    return res.status(500).json({ message: "user already exist" });
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      return res.status(500).json({ message: "user already exist" });
+    }
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const user = await User.create({ email, password: hashedPassword });
+    res.status(201).json({ message: "Registered" });
+  } catch (error) {
+    res.status(500).json({ message: "server error", error });
   }
-  const hashedPassword = await bcrypt.hash(password, 10);
-
-  const user = await User.create({ email, password: hashedPassword });
-  res.status(201).json({ message: "Registered" });
 };
 export const login = async (req, res) => {
-  const { email, password } = req.body;
-  
-  const existingUser = await User.findOne({ email });
-  if (!existingUser) {
-    return res.status(400).json({ message: "wrong email or password" });
+  try {
+    const { email, password } = req.body;
+
+    const existingUser = await User.findOne({ email });
+    if (!existingUser) {
+      return res.status(400).json({ message: "wrong email or password" });
+    }
+    const isMatch = await bcrypt.compare(password, existingUser.password);
+    if (!isMatch) {
+      return res.status(400).json({ message: "wrong email or password" });
+    }
+    res.status(200).json({ message: "LoggedIn" });
+  } catch (error) {
+    res.status(500).json({ message: "server error", error });
   }
-  const isMatch = await bcrypt.compare(password, existingUser.password);
-  if (!isMatch) {
-    return res.status(400).json({ message: "wrong email or password" });
-  }
-  res.status(200).json({ message: "LoggedIn" });
 };
--- controllers/pactController.js
+++ controllers/pactController.js
@@ -1,18 +1,64 @@
 import Pact from "../models/pact.js";
 
 export const getPacts = async (req, res) => {
-  res.send("get all pacts");
+  try {
+    const pacts = await Pact.find();
+    if (pacts.length === 0) {
+      return res.status(404).json({ message: "no pacts avilable" });
+    }
+    res.status(200).json({ pacts });
+  } catch (error) {
+    res.status(500).json({ message: "server error", error });
+  }
 };
 
 export const createPact = async (req, res) => {
-  res.send("created a pact");
+  try {
+    const newPact = req.body;
+    if (!newPact) {
+      return res.status(400).json({ message: "something went wrog" });
+    }
+    const pact = await Pact.create(newPact);
+    res.status(201).json({ message: "pact has been created", pact });
+  } catch (error) {
+    res.status(500).json({ message: "Server error", error });
+  }
 };
 export const getPact = async (req, res) => {
-  res.send("get a pact");
+  try {
+    const { id } = req.params;
+    const pact = await Pact.findOne({ _id: id });
+    if (!pact) {
+      return res.status(404).json({ message: "this pact does not exist" });
+    }
+    res.status(200).json({ message: "found it", pact: pact });
+  } catch (error) {
+    res.status(500).json({ message: "server error", error });
+  }
 };
 export const updatePact = async (req, res) => {
-  res.send("update a pact");
+  try {
+    const { id } = req.params;
+    const updatedPact = await Pact.findOneAndUpdate({ _id: id }, req.body, {
+      new: true,
+    });
+    if (!updatedPact) {
+      return res.status(404).json({ message: "this pact does not exist" });
+    }
+    res.status(200).json({ message: "update succesfully", updatedPact });
+  } catch (error) {
+    res.status(500).json({ message: "server error", error });
+  }
 };
 export const deletePact = async (req, res) => {
-  res.send("delete a pact");
+  try {
+    const { id } = req.params;
+    const pact = await Pact.findOneAndDelete({ _id: id });
+    if (!pact) {
+      return res.status(404).json({ message: "this pact does not exist" });
+    }
+    res.status(204);
+  } catch (error) {
+    res.status(500).json({ message: "server error", error });
+  }
 };
